62b663b57a61402ae6755e2f8c13e4361b6d96ef
galanggg <tugasgalang@gmail.com>
2022-12-25 12:18:35 +0800

    initial learning ✨

---
+++ src/routes/contacts/index.tsx
@@ -0,0 +1,37 @@
+import { component$, Resource, useSignal, useStylesScoped$ } from "@builder.io/qwik";
+import { RequestHandler, useEndpoint } from "@builder.io/qwik-city";
+import { Contact, CONTACTS } from "./fake-db";
+import CSS from './index.css?inline'
+
+export const onGet: RequestHandler<Contact[]> = async () => {
+    return await Promise.resolve(CONTACTS)
+}
+
+export default component$(() => {
+    useStylesScoped$(CSS)
+    const endpoint = useEndpoint<typeof onGet>()
+    const filter = useSignal('')
+    return (
+        <div>
+            <h1>Contacts</h1>
+            <input placeholder="Search contacts.." onInput$={(event) => {filter.value = (event.target as HTMLInputElement).value}} />
+
+            <p>Here some contacts</p>
+            <Resource
+                value={endpoint}
+                onPending={() => <div>Loading...</div>}
+                onResolved={(contacts) => {
+                    return <ul>{contacts.filter(c => c.name.toLowerCase().indexOf(filter.value.toLowerCase()) > -1).map(contact => {
+                        return (
+
+                            <li>
+                                <img src={contact.avatar} />
+                                {contact.name}
+                            </li>
+                        )
+                    })}</ul>
+                }}
+            />
+        </div>
+    )
+})
